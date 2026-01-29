e47b68b1bb237ce6f01baa5469556447795816f4
Kaushal Bhavsar <kaushalbhavsar05@gmail.com>
2026-01-29 21:43:13 +0530

    Week 4 modules, exception handling programs and regression models notebook

---
+++ week-04/programs/use_math_module.py
@@ -0,0 +1,5 @@
+import math_module as maths
+result_add = maths.add(5,3)
+result_subtract = maths.subtract(10,4)
+print("Addition Result:", result_add)
+print("Subtraction Result:", result_subtract)
